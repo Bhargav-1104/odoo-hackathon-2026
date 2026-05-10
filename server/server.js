cb9f4ad557a4b9235f89646706e4858b6b7690a4
Bhargav-1104 <bhargavsharma1104@gmail.com>
2026-05-10 11:56:01 +0530

    Completed full-stack authentication flow integration

--- server/server.js
+++ server/server.js
@@ -9,6 +9,16 @@ import {
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+  next();
+});
+
 app.use(express.json());
 
 app.get("/health", (_req, res) => {
